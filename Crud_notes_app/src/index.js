a03856ff779bb705564e63a88605ecef792717c6
Juan Carlos Arias Botero <jacal2211@gmail.com>
2020-01-25 14:16:50 -0500

    Actualizado Enero 25 del 2020, 2:16 pm

---
+++ Crud_notes_app/src/index.js
@@ -0,0 +1,38 @@
+//Initializations
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride=require('method-override');
+const session = require('express-session');
+//Initializations
+const app = express();
+//Settings
+app.set('port',process.env.PORT||3000);
+app.set('views',path.join(__dirname, 'views'));
+app.engine('.hbs',exphbs({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: 'hbs',
+}));
+app.set('view engine','hbs')
+//Middlewares
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'mysecretapp',
+    saveUninitialized: true,
+    resave:true
+}));
+//Global Variables
+
+//Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
+//Static Files
+app.use(express.static(path.join(__dirname,'public')));
+//Server is Listenning
+app.listen(app.get('port'), ()=>{
+    console.log('Server on Port',app.get('port'));
+});
